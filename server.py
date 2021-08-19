09b5bc46fb324e6676501758fa8446ec66964d6f
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-08-19 22:45:57 +0000

    Added routes and template files

--- server.py
+++ server.py
@@ -12,8 +12,56 @@ API_KEY = os.environ['LASTFM_KEY']
 
 @app.route("/")
 def index():
+    """Landing page"""
   
     return render_template("homepage.html")
+
+#############################################
+# USER LOGIN INFO
+@app.route("/signup")
+def signup_user():
+    """Shows user sign up form"""
+
+    return render_template("signup.html")
+
+@app.route("/login")
+def login_user():
+    """Shows user login form"""
+
+    return render_template("login.html")
+
+
+#############################################
+@app.route("/user-profile")
+def show_profile():
+    """Displays user profile"""
+
+    return render_template("profile.html")
+
+
+@app.route("/new-playlist")
+def generate_playlist():
+    """Displays generated playlist"""
+
+    return render_template("new_playlist.html")
+
+@app.route("/saved-playlists")
+def show_playlists():
+    """Displays saved playlists saved by user"""
+
+    return render_template("playlists.html")
+
+@app.route("/about-radlist")
+def show_about():
+    """Displays about page for RAD List"""
+
+    return render_template("about_radlist.html")
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
