ba75a65eda11e1950a1e1832ff715975e6b2ba5b
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-08-30 06:49:58 +0000

    Added and tested functions

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 """CRUD operations."""
 
 from model import db, User, Track, Playlist, Playlist_Track, connect_to_db
+from flask import session
 from passlib.hash import argon2
 
 
@@ -44,6 +45,16 @@ def create_track(title, artist):
     
     return track
 
+def create_playlist_track(playlist_id, track_id):
+    """ Creates a playlist track for a specific playlist """
+    
+    playlist_track = Playlist_Track(playlist_id = playlist_id, 
+                                    track_id = track_id)
+
+    db.session.add(playlist_track)
+    db.session.commit()
+
+    return playlist_track
 
 
 def get_user_by_email(email):
@@ -55,23 +66,23 @@ def get_user_by_email(email):
 def get_user_playlist(user):
     """ Displays user playlists """
 
-    return User.query.filter(User.playlists == user).all()
+    return Playlist.query.filter(Playlist.user_id == user).all()
 
 
 
 def get_track(track_id):
     """ Pulls a track """
 
-    return Track.query.filter(Track.track_id == track_id).first()
+    return Track.query.get(track_id)
 
 
-def add_playlist_to_user(playlist, user): 
-    """ Adds playlist """
 
-    user.playlists.append(playlist)
+# def add_playlist_to_user(playlist, user): 
+#     """ Adds playlist """
 
-    db.session.commit()
+#     user.playlists.append(playlist)
 
+#     db.session.commit()
 
 
 def get_playlist_by_id(playlist_id):
@@ -80,25 +91,47 @@ def get_playlist_by_id(playlist_id):
     return Playlist.query.filter(Playlist.playlist_id == playlist_id).first()
 
 
-def add_track_to_playlist(playlist_id, track_id):
-    """ Add track to playlist """
-    get_playlist_by_id(playlist_id)
-    get_track(track_id)
+# def add_track_to_playlist(playlist_id, track_id):
+#     """ Add track to playlist """
+    
+#     pl = get_playlist_by_id(playlist_id)
+#     pl.append(track_id)
 
-pass
+    # db.session.commit()
 
-def create_user_playlist(playlist_dict):
+def get_track_id(title):
+    """ Gets track_id by title of song """
+    # prob need to also include artist here bc dupe titles
+    return Track.query.filter(Track.title == title).first()
+
+
+def get_playlist_tracks(playlist_id):
+    """ Gets list of all tracks in a user playlist """
+
+    return Playlist_Track.query.filter(Playlist_Track.playlist_id == playlist_id).all()
+
+##########################################################
+
+def create_user_playlist(track_info):
     """ Creates playlist saved by user """
-    #before for loop, call create playlist fcn
-    #after creating tracks in loop
+
     #add a track to a playlist -- need to create this function
     #add to association table 
     #get paylist id and trackid just made and add to association table
-    create_playlist(user, 'My Saved Playlist')
+    # user = get_user_by_email(session['EMAIL'])
+    user = get_user_by_email('pollo@cat.com')
+    new_playlist = create_playlist(user, 'Fresh New Playlist')
 
-    for name, track in playlist_dict.items():
-        create_track(track[0], name)
-        create_track(track[1], name)
+    for song in track_info.values():
+        
+        print(song[0])
+        print(song[1])
+        # track_id2 = get_track_id(track[1])
+        # create_playlist_track(playlist_id, track_id)
+        # create_playlist_track('4', track_id1)
+        # # create_playlist_track('4', track_id2)
+       
+            
 
 
 if __name__ == '__main__':
