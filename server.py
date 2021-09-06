97742558dcf7c54ce897ed6fa27aa0b231c63586
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-09-06 05:43:31 +0000

    Modified routes, added jasonify, added track duration

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 """ Server for RAD List """
 
-from flask import Flask, render_template, redirect, request, session, flash
+from flask import Flask, render_template, redirect, request, session, flash, jsonify
 from jinja2 import StrictUndefined
 from model import connect_to_db, User, Playlist, Track
 import requests
 import crud 
+import helper
 import os
 from passlib.hash import argon2
-
+import json
 
 
 app = Flask(__name__)
@@ -21,20 +22,15 @@ LASTFM_API_KEY = os.environ['LASTFM_KEY']
 @app.route('/')
 def index():
     """ Display homepage """
-
-    return render_template('homepage.html')
-
-
-
-@app.route('/signup', methods=['GET'])
-def show_signup():
-    """ Displays user signup form """
-
-    return render_template('signup.html')
+    if session:
+        return redirect('/search')
+    
+    else:
+        return render_template('homepage.html')
 
 
 
-@app.route('/signup', methods=['POST'])
+@app.route('/users/create-user.json', methods=['POST'])
 def signup_user():
     """ Process user signup form """
     
@@ -48,22 +44,16 @@ def signup_user():
     # If email exists in db, redirect to homepage
     if user != None:
         flash('This email is already taken.')
-        return redirect('/signup')
+        return jsonify('This email is already taken.')
 
     # If user email/pass is already in the db
     elif user != None and user.email == email and user.password == password:
         flash('This email and password already exists. Please go to Login to sign in.')
-
+        return jsonify('This email and password already exists. Please go to Login to sign in.')
     # If a new user and all correct
     else:
         # Add new user
-        new_user = crud.create_user(fname = fname, 
-                                    lname = lname, 
-                                    email = email,
-                                    password = password)
-       
-        crud.db.session.add(new_user)
-        crud.db.session.commit()
+        new_user = crud.create_user(fname, lname, email, password)
 
         # Saving user to session
         user = crud.get_user_by_email(email) 
@@ -71,17 +61,8 @@ def signup_user():
         session['FNAME'] = user.fname 
         session['LNAME'] = user.lname
         session['ID'] = user.user_id
-        print('success')
         flash('You have registered successfully.')
-        return redirect ('/')
-
-
-
-@app.route('/login', methods=['GET']) 
-def show_login():
-    """ Shows user login form """
-
-    return render_template('login.html')
+        return jsonify('You have registered successfully. Please log in.')
 
 
 
@@ -99,7 +80,7 @@ def process_login():
     # If user does not exist
     if user == None:
         flash('Email does not exist. Please sign up or try again.')
-        return redirect('/login')
+        return redirect('/')
     
     # If user exists and password matches, show success and take to profile
     else:
@@ -109,44 +90,44 @@ def process_login():
             session['LNAME'] = user.lname
             session['ID'] = user.user_id
             session['saved_playlist'] = None
-            print('success')
             flash('You have successfully logged in.')
-            return redirect ('/')
+            return redirect ('/search')
 
         else:
             flash('Incorrect Password. Please try again.')
-            return redirect ('/login')   
+            return redirect ('/')   
       
-    
 
-@app.route ('/logout')
+
+@app.route('/search')
+def search_artists():
+    """ Search for similar artists """
+
+    return render_template('searchhome.html')
+
+
+
+@app.route('/logout')
 def logout_user():
     """ Logs out user """
 
     session.clear()
 
     return redirect('/')
+ 
 
 
-
-@app.route('/profile')
-def show_user_profile():
+@app.route('/users/profile/<fname>')
+def show_user_profile(fname):
     """ Displays user profile """   
 
-    # If user clicks on profile and not signed in   
     if not session:
         flash('Please login to view profile.')
         return redirect('/login')
 
     else:
-        return render_template('profile.html')
+        return render_template('profile.html', fname=fname)
 
-
-
-
-# For /new-playlist and /playlists routes
-TOP_TRACKS = []
-SIMILAR_ARTISTS_LIST = []
 
 
 @app.route('/new-playlist', methods=['POST'])
@@ -157,7 +138,7 @@ def generate_playlist():
     form_artist = request.form.get('form_artist')
     
 
-    # LAST FM endpoint for getting similar artists
+    # LAST FM endpoint for getting similar artists - API Call #1
     url1 = 'http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar'
 
 
@@ -170,20 +151,20 @@ def generate_playlist():
     data = response.json()
    
 
+    top_track_list = []
+    similar_artists_list = []
 
     # List of 15 similar artists for template
     for i in range (15):
         similar_artists = (data['similarartists']['artist'][i])['name']
-        SIMILAR_ARTISTS_LIST.append(similar_artists)
-   
+        similar_artists_list.append(similar_artists)
+        similar_artists_list.append(similar_artists)
     
-
-    # LAST FM endpoint for getting artist's Top Tracks
+    
+    # LAST FM endpoint for getting artist's Top Tracks - API Call #2
     url2 = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks'
 
     
-    # A dict of artists and tracks
-    artists = {}
     
     # Loops through the similar artists list and inserts into payload
     for artist in data['similarartists']['artist']:
@@ -203,57 +184,80 @@ def generate_playlist():
         # Top 2 Tracks
         track_name_1 = artist_data['toptracks']['track'][0]['name']
         track_name_2 = artist_data['toptracks']['track'][1]['name']
-        TOP_TRACKS.append(track_name_1)
-        TOP_TRACKS.append(track_name_2)
+        top_track_list.append(track_name_1)
+        top_track_list.append(track_name_2)
 
 
-       
-        crud.create_track(track_name_1, artist['name'])
-        crud.create_track(track_name_2, artist['name'])
-        
-
-   
-    return render_template('new_playlist.html', 
-                            data=artists,
-                            SIMILAR_ARTISTS_LIST=SIMILAR_ARTISTS_LIST,
-                            TOP_TRACKS=TOP_TRACKS)
-
-
-
-# Not an intended feature but keeping for now
-@app.route('/newplaylist/details', methods=['GET'])
-def show_last_search():
-    """ Displays last search """
-
-    if session and session['saved_playlist'] == None:
-        flash('You must first save a playlist to view saved playlists.')
-        return redirect("/")
+    # Create dictionary of new playlist output for API Call #3
+    sim_artists_and_top_tracks = {}
+    for i in range(len(similar_artists_list)):
+        if similar_artists_list[i] in sim_artists_and_top_tracks:
+            sim_artists_and_top_tracks[similar_artists_list[i]].append(top_track_list[i])
+        else:
+            sim_artists_and_top_tracks[similar_artists_list[i]] = [top_track_list[i]]
     
-    if not session:
-        flash('You must sign in to save/view playlists.')
-        return redirect("/")
 
-    return render_template("/playlistdetails.html")
+    track_duration_list = [] # Times in milliseconds. need to convert with crud fcn.
 
+     # LAST FM endpoint for getting artist's Top Tracks - API Call #3
+    url3 = 'http://ws.audioscrobbler.com/2.0/?method=track.getInfo'
+
+    for artist in sim_artists_and_top_tracks.keys():
+        for track in sim_artists_and_top_tracks[artist]:
+
+        
+            payload3 = {'api_key': LASTFM_API_KEY,
+                        'artist': artist,
+                        'track': track,
+                        'format': 'json'}
+
+            response3 = requests.get(url3, params=payload3)
+            track_dur_data = response3.json() 
+            
+            track_duration = track_dur_data['track']['duration']
+
+            track_duration_list.append(track_duration)
+    
+    # Convert tracks from milliseconds to 00:00:00 format
+    conv_track_lengths = crud.convert_millis(track_duration_list)
+    
+    # similar_artists_list = []
+    # top_track_list = []
+    # conv_track_lengths = []
+    playlist_query_list = helper.create_artist_track_dur_list(similar_artists_list, top_track_list, conv_track_lengths)
+   
+    # Add tracks to db
+    for i in range(len(top_track_list)):
+        crud.create_track(top_track_list[i], similar_artists_list[i], conv_track_lengths[i])
+
+    # Save playlist info to session 
+    session['queried_playlist'] = playlist_query_list
+    
+    
+    # Track times will show but not saved in session/tracks
+    return render_template('new_playlist.html', 
+                            similar_artists_list=similar_artists_list,
+                            top_track_list=top_track_list,
+                            conv_track_lengths=conv_track_lengths)
 
 
 
 @app.route('/playlists', methods=['GET'])
 def show_saved_playlist():
     """ Displays list of saved playlists """
+    # Add logic if user not in session flash msg to log in
+    if not session:
+        flash('You must sign in to save/view playlists.')
+        return redirect("/")
 
-     
     playlists = crud.get_saved_playlists(crud.get_user_by_email(session['EMAIL']).user_id)
     playlist_ids = crud.get_user_playlist_ids(crud.get_user_by_email(session['EMAIL']).user_id)
     
     playlists_and_playlist_ids = {}
-    
-    for playlist in playlists:
-        for playlist_id in playlist_ids:
-            playlists_and_playlist_ids[playlist] = playlist_id
-    
-   # Jinja template - all links have same playlist_id
-    
+
+    for i in range(len(playlists)):
+        playlists_and_playlist_ids[playlists[i]] = playlist_ids[i]
+
 
     return render_template('savedplaylists.html', 
                             playlists_and_playlist_ids=playlists_and_playlist_ids)
@@ -271,7 +275,15 @@ def save_my_playlist():
     saved_playlist = crud.create_playlist(crud.get_user_by_email(session['EMAIL']), playlist_name)
     playlist_id = saved_playlist.playlist_id
 
-    for track in TOP_TRACKS:
+    # get tracks from queried session playlist
+    playlist = session['queried_playlist'] 
+    
+    playlist_track_list = [] 
+
+    for tracks in playlist:
+        playlist_track_list.append(tracks[1])
+    
+    for track in playlist_track_list:
         crud.create_playlist_track(saved_playlist.playlist_id, crud.get_track_id(track))
 
 
@@ -286,31 +298,27 @@ def show_playlist_by_id(playlist_id):
     playlist_name = crud.get_playlist_name(playlist_id)
 
     track_objs = crud.get_playlist_tracks(playlist_id)
-
-    # artists = {}
-    # Write dictionary instead of appending to two lists?
-    # Values can be overwritten - how to add to values 
-    # for track in track_obj:
-    #     artist = crud.get_artist_by_track_id(track.track_id)   
-    #     track = crud.get_title_by_track_id(track.track_id)
-    #     artists[artist] = track
-    #     artists[artist] = []
-    #     artists[artist].append(track)
-    
     
     tracks = []
     artists = []
+    track_durs = []
+
     for track in track_objs:
         artist = crud.get_artist_by_track_id(track.track_id) 
         track = crud.get_title_by_track_id(track.track_id)  
         artists.append(artist)
         tracks.append(track)
-    
+       
+    # Had to separate from above loop for track time
+    for track in track_objs:
+        dur = crud.get_track_dur(str(track.track_id))
+        track_durs.append(dur)
 
     return render_template('playlist_id.html', 
                             playlist_name=playlist_name,
                             artists=artists,
-                            tracks=tracks)
+                            tracks=tracks,
+                            track_durs=track_durs)
 
 
 
@@ -319,6 +327,7 @@ def show_about():
     """Displays about page for RAD List"""
 
     return render_template('about_radlist.html')
+
 
 
 
