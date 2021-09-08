afca5d404d7f35ddf23b87f1c57405f47b33c572
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-09-08 10:02:51 +0000

    Added event handler to update profile with playlist links

--- static/js/user_profile.js
+++ static/js/user_profile.js
@@ -4,3 +4,20 @@ $.get('/users/profile/api', (data) => {
     $('#user-name').text(`${data.fname} ${data.lname}`);
     $('#user-email').text(data.email);
 });
+
+
+const savedPlaylistsLink = $('#saved-playlists-link');
+const savedPlaylist = $('#playlist-links');
+
+$('#list').hide();
+
+savedPlaylistsLink.on('click', (evt) => {
+    evt.preventDefault();
+    
+    savedPlaylistsLink.hide();
+    $('#list').show();
+
+
+});
+
+
