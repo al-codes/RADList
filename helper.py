c456421b51c6703ca66775cf7eb30fc74ea9ca19
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-09-08 10:01:11 +0000

    Added functions to help clean up server

--- helper.py
+++ helper.py
@@ -7,9 +7,7 @@ def create_artist_track_dur_list(artist_list, track_list, dur_list):
 
     for i in range(len(artist_list)):
         master.append([artist_list[i], track_list[i], dur_list[i]])
-
     return master   
-
 
 
 def convert_millis(track_dur_lst):
@@ -21,8 +19,29 @@ def convert_millis(track_dur_lst):
         minutes = int(int(track_dur)/60000)
         hours = int(int(track_dur)/(60000*60))
         converted_time = '%02d:%02d:%02d' % (hours, minutes, seconds)
-        converted_track_times.append(converted_time)
-            
+        converted_track_times.append(converted_time)    
     return converted_track_times
    
+
+def create_dict_sim_artists_top_tracks(sa_list, t_tracks):
+    """ Creates a dictionary of similar artists and 2 top tracks """
+
+    sim_artists_and_top_tracks = {}
+
+    for i in range(len(sa_list)):
+        if sa_list[i] in sim_artists_and_top_tracks:
+            sim_artists_and_top_tracks[sa_list[i]].append(t_tracks[i])
+        else:
+            sim_artists_and_top_tracks[sa_list[i]] = [t_tracks[i]]
+    return sim_artists_and_top_tracks   
+
+
+def create_dict_playlists_playlistids(p_list, pid_list):
+    """ Create a dictionary of playlists and playlist ids """
+
+    playlists_and_playlist_ids = {}
+
+    for i in range(len(p_list)):
+        playlists_and_playlist_ids[p_list[i]] = pid_list[i]
+    return playlists_and_playlist_ids
 
