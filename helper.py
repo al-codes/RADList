c480bbda4855c12e61a7a754b3e3c54b74bd8a48
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-09-11 20:47:35 +0000

    Added function to make dict out of sim artists, tracks and track duration

--- helper.py
+++ helper.py
@@ -36,6 +36,19 @@ def create_dict_sim_artists_top_tracks(sa_list, t_tracks):
     return sim_artists_and_top_tracks   
 
 
+def create_dict_sim_artists_top_tracks_duration(sa_list, t_tracks, dur_list):
+    """ Create a dictionary of similar artists, top tracks and track duration """
+
+    master = {}
+
+    for i in range(len(sa_list)):
+        if sa_list[i] in master:
+            master[sa_list[i]].extend([t_tracks[i], dur_list[i]])
+        else:
+            master[sa_list[i]] = [t_tracks[i], dur_list[i]]
+    return master
+
+
 def create_dict_playlists_playlistids(p_list, pid_list):
     """ Create a dictionary of playlists and playlist ids """
 
@@ -45,3 +58,4 @@ def create_dict_playlists_playlistids(p_list, pid_list):
         playlists_and_playlist_ids[p_list[i]] = pid_list[i]
     return playlists_and_playlist_ids
 
+    
