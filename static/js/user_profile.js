60fe342f4cd8cedf070b85bbfa6278a23617bfe4
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-09-16 06:07:30 +0000

    Added comment for function

--- static/js/user_profile.js
+++ static/js/user_profile.js
@@ -1,11 +1,12 @@
 "use strict"
 
+// Profile details
 $.get('/users/profile/api', (data) => {
     $('#user-name').text(`${data.fname} ${data.lname}`);
     $('#user-email').text(data.email);
 });
 
-
+// Shows saved playlists links on profile
 const savedPlaylistsLink = $('#saved-playlists-link');
 const savedPlaylist = $('#playlist-links');
 
@@ -16,7 +17,6 @@ savedPlaylistsLink.on('click', (evt) => {
     
     savedPlaylistsLink.hide();
     $('#list').show();
-
 
 });
 
