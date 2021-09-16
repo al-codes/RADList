8218526125b83002d16db7cec523ef39424c422e
Alexandra Sanchez <alexlsanchez@gmail.com>
2021-09-16 06:15:45 +0000

    Removed empty file

---
+++ static/js/newplaylisttable.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// const savePlaylistForm = $('#save-playlist-btn');
+// const savedPlaylistName= $('#saved-playlist-name')
+// const newPlaylistName = $('#user-playlist-name')
+
+// savePlaylistForm.on('click', (evt) => {
+//     savePlaylistForm.hide();
+//     savedPlaylistName.show();
+
+// });
